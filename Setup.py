66c0a7a9a46283282b8b3a4415e9a6a06713aea5
Rosutovein <alexandre.pigaa@gmail.com>
2021-11-02 14:13:12 +0100

    AP: Mise à jour du fichier Setup.py

--- Setup.py
+++ Setup.py
@@ -1,4 +1,25 @@
 import os
 
+os.system('apt-get update')
+os.system('apt upgrade')
+os.system('apt-get install -y python3-pip')
 os.system('git submodule update --init --recursive')
 os.system('pip3 install h8mail')
+#Requirements installation
+root = os.path.join('..', 'OSINT')
+path = os.getcwd()
+for directory, subdir_list, file_list in os.walk(root):
+    for name in file_list:
+        if(name == "requirements.txt"):
+            os.chdir(directory)
+            os.system('python3 -m pip install -r ' + name)
+            os.chdir(path)
+#Setup installation
+for directory, subdir_list, file_list in os.walk(root):
+    for name in file_list:
+        if(name == "setup.py"):
+            os.chdir(directory)
+            os.system('python3 ' + name + ' install')
+            os.chdir(path)
+#os.system('pip3 install requests_futures')
+#os.system('pip3 install torrequest')
